be4a3cffe024a4a23b6695d91fe2df5e61714e6d
Guilherme Cajeira <gcajeira@ua.pt>
2023-12-16 23:43:25 +0000

    Update primeira versão controlador temperatura

--- AppTempSystem.py
+++ AppTempSystem.py
@@ -11,30 +11,24 @@ class TemperatureControlApp(QWidget):
         super(TemperatureControlApp, self).__init__()
 
         self.temperature_sensor = TemperatureSensor()
-        self.temp_controller = TemperatureController(25.0)
+        self.temp_controller = TemperatureController(self.temperature_sensor)
 
         self.init_ui()
 
     def init_ui(self):
         main_layout = QVBoxLayout()
 
-
-
         # Group Box 1 - Display and Input
         group_box1 = QGroupBox("Display")
         layout1 = QVBoxLayout()
 
         self.label_temperature = QLabel(f"Current Temperature: {self.temperature_sensor.current_temperature}°C")
-        # self.entry_temperature = QLineEdit()
         self.label_operation_mode = QLabel("Operation Mode: None")
 
         layout1.addWidget(self.label_temperature)
-        # layout1.addWidget(self.entry_temperature)
         layout1.addWidget(self.label_operation_mode)
 
         group_box1.setLayout(layout1)
-
-
 
         # Group Box 2 - Operation Modes
         group_box2 = QGroupBox("Operation Modes")
@@ -59,9 +53,6 @@ class TemperatureControlApp(QWidget):
 
         group_box2.setLayout(layout2)
 
-
-
-
         # Add Group Boxes to the main layout
         main_layout.addWidget(group_box1)
         main_layout.addWidget(group_box2)
@@ -84,35 +75,41 @@ class TemperatureControlApp(QWidget):
         # Set background color to light blue
         self.setStyleSheet("background-color: lightblue;")
 
-
-
     def choose_temperature(self):
         temperature, ok = QInputDialog.getDouble(self, "Choose Temperature", "Enter desired temperature:")
         if ok:
-            self.temp_controller.desired_temperature = temperature
-            self.label_operation_mode.setText(f"Operation Mode: Choose Temperature ({self.temp_controller.desired_temperature}°C)")
-            # self.update_temperature_display()
-            QMessageBox.information(self, "Temperature Control System", f"Desired temperature set to {temperature}°C")     
+            # self.temp_controller.set_desired_temperature(temperature)
+            # self.label_operation_mode.setText(f"Operation Mode: Choose Temperature ({self.temp_controller.desired_temperature}°C)")
+            # QMessageBox.information(self, "Temperature Control System", f"Desired temperature set to {temperature}°C")
+
+            self.temp_controller.activate_controller(temperature)
+            self.label_operation_mode.setText(f"Operation Mode: Choose Temperature ({temperature}°C)")
+            QMessageBox.information(self, "Temperature Control System", f"Desired temperature set to {temperature}°C")
+
 
     def auto_regulate_temperature(self):
+        self.temp_controller.controller_active = False
         self.temp_controller.auto_regulate_temperature()
         self.update_temperature_display()
         self.label_operation_mode.setText("Operation Mode: Auto Regulate")
 
     def heat_room(self):
+        self.temp_controller.controller_active = False
         self.temp_controller.heat_room()
         self.update_temperature_display()
         self.label_operation_mode.setText("Operation Mode: Heat Room")
 
     def cool_room(self):
+        self.temp_controller.controller_active = False
         self.temp_controller.cool_room()
         self.update_temperature_display()
         self.label_operation_mode.setText("Operation Mode: Cool Room")
 
     def update_temperature_display(self):
+        self.temp_controller.adjust_temperature()
         current_temperature = self.temperature_sensor.read_temperature()
         self.label_temperature.setText(f"Current Temperature: {current_temperature:.2f}°C")
-        self.temp_controller.adjust_temperature(current_temperature)
+
 
 
 if __name__ == '__main__':
--- TempControlSystem.py
+++ TempControlSystem.py
@@ -4,52 +4,82 @@ import random
 class TemperatureSensor:
     def __init__(self):
         self.current_temperature = 20.0
+        self.temperature_variation = 0.0
 
     def read_temperature(self):
+        # Adiciona a variação causada pelo controlador ao valor atual da temperatura
+        self.current_temperature += self.temperature_variation
+
+        # Simula uma leitura real do sensor
         variation = random.uniform(-0.5, 0.5)
         self.current_temperature += variation
+
         return self.current_temperature
 
+    def set_temperature_variation(self, variation):
+        # Define a variação de temperatura causada pelo controlador
+        self.temperature_variation = variation
+
+
+class PIDController:
+    def __init__(self, setpoint):
+        self.setpoint = setpoint
+        self.Kp = 0.5  # Constante proporcional
+        self.Ki = 0.2  # Constante integral
+        self.Kd = 0.01 # Constante derivativa
+        self.prev_error = 0.0
+        self.integral = 0.0
+
+    def update(self, current_temperature):
+        error = self.setpoint - current_temperature
+        print(error)
+
+        self.integral += error
+        derivative = error - self.prev_error
+
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+
+        print(output)
+        self.prev_error = error
+
+        return output
+
+
 class TemperatureController:
-    def __init__(self, desired_temperature):
+    def __init__(self, temperature_sensor):
+        self.desired_temperature = 25.0
+        self.pid_controller = PIDController(self.desired_temperature)
+        self.temperature_sensor = temperature_sensor
+        self.controller_active = False
+
+    def activate_controller(self, desired_temperature):
         self.desired_temperature = desired_temperature
-        self.target_temperature_range = 1.0
-        self.air_conditioner_active = False
-        self.heater_active = False
+        self.pid_controller.setpoint = desired_temperature
+        self.controller_active = True
 
-    def adjust_temperature(self, current_temperature):
-        temperature_difference = current_temperature - self.desired_temperature
+    def adjust_temperature(self):
+        if self.controller_active:
+            current_temperature = self.temperature_sensor.read_temperature()
+            pid_output = self.pid_controller.update(current_temperature)
 
-        if abs(temperature_difference) > self.target_temperature_range:
-            if temperature_difference > 0:
-                self.activate_air_conditioner()
-                self.deactivate_heater()
+            if pid_output > 0:
+                print("Temperature increasing...")
+            elif pid_output < 0:
+                print("Temperature decreasing...")
             else:
-                self.activate_heater()
-                self.deactivate_air_conditioner()
-        else:
-            self.deactivate_air_conditioner()
-            self.deactivate_heater()
+                print("Temperature stable.")
 
-    def activate_air_conditioner(self):
-        if not self.air_conditioner_active:
-            print("Air conditioner activated")
-            self.air_conditioner_active = True
+            # Ajustar a temperatura da sala com base na saída do PID
+            self.temperature_sensor.set_temperature_variation(pid_output)
 
-    def deactivate_air_conditioner(self):
-        if self.air_conditioner_active:
-            print("Air conditioner deactivated")
-            self.air_conditioner_active = False
+            if abs(current_temperature - self.desired_temperature) < 0.1:
+                print("Temperature reached the setpoint!")
 
-    def activate_heater(self):
-        if not self.heater_active:
-            print("Heater activated")
-            self.heater_active = True
+            
 
-    def deactivate_heater(self):
-        if self.heater_active:
-            print("Heater deactivated")
-            self.heater_active = False
+    def set_desired_temperature(self, new_target):
+        self.desired_temperature = new_target
+        self.pid_controller.setpoint = new_target
 
     def auto_regulate_temperature(self):
         self.heating = False
